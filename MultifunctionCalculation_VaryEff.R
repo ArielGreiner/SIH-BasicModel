c747cda2c35c4fc8de7e1836e25abfec2ad7dc9d
ArielGreiner <ariel.kiara@gmail.com>
2015-11-11 19:16:28 -0500

    Fixed it so that it will work for one function only.  Haven't added replicates yet

--- MultifunctionCalculation_VaryEff.R
+++ MultifunctionCalculation_VaryEff.R
@@ -1,6 +1,6 @@
 #source("SIH code_PLT.r") #the SIH model code
 require(vegan)
-SIH_data<-sapply(DispV,SIH_function,species=nspecies,patches=npatches,eff_vary=T,eff_values=eff_values)
+#SIH_data<-sapply(DispV,SIH_function,species=nspecies,patches=npatches,eff_vary=T,eff_values=eff_values)
 
 nspecies<-9 #the number of species
 npatches<-30 #the number of patches
@@ -25,11 +25,14 @@ Function_rates<-data.frame(Rate=NA,Function=factor(1:nfunctions),Dispersal=rep(D
 Function_number<-data.frame(Number=NA,Dispersal=DispV,Scale=rep(c("Local","Regional"),each=length(DispV))) #storage dataframe for functional rates
 
 for(i in 1:length(DispV)){ #loops through all dispersal rates
-  LFunc_rate1<-apply(SIH_data[["Abund",i]],1,function(M){MTraits%*%M}) #calculates the amount of each function based on the abundances from the SIH model
+  LFunc_rate1<-apply(SIH_data[["Abund",i]],1,function(M){MTraits%*%M}) #calculates the amount of each function in each patch based on the abundances from the SIH model
   LFunc_rate<-array(t(LFunc_rate1),dim=c(nrow(SIH_data[["Abund",1]]),nfunctions,npatches)) #arranges the functions into an array that is easier to use
   Function_number[Function_number$Scale=="Local" & Function_number$Dispersal==DispV[i],"Number"]<-mean(apply(LFunc_rate>0,3,rowSums))
   Function_number[Function_number$Scale=="Regional" & Function_number$Dispersal==DispV[i],"Number"]<-mean(rowSums(apply(LFunc_rate,2,rowSums)>0))
-  Function_rates[Function_rates$Scale=="Local" & Function_rates$Dispersal==DispV[i],"Rate"]<-rowMeans(apply(LFunc_rate,3,colMeans)) #calculates and saves the mean local rate for each function
+  #Single function variant:
+  Function_rates[Function_rates$Scale=="Local" & Function_rates$Dispersal==DispV[i],"Rate"]<-mean(apply(LFunc_rate,3,colMeans))
+  #multifunction variant:
+  #Function_rates[Function_rates$Scale=="Local" & Function_rates$Dispersal==DispV[i],"Rate"]<-rowMeans(apply(LFunc_rate,3,colMeans)) #calculates and saves the mean local rate for each function
   Function_rates[Function_rates$Scale=="Regional" & Function_rates$Dispersal==DispV[i],"Rate"]<-colMeans(apply(LFunc_rate,2,rowSums)) #calculates and saves the mean regoinal rate for each function
 }
 
